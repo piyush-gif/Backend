f80f3c936128d28446588f23eee1be431b7cebbd
piyush <piyush.khadka2233@gmail.com>
2026-01-21 17:17:57 +0000

    get requests

---
+++ app/storage.py
@@ -0,0 +1,4 @@
+from pathlib import  Path
+import json
+
+DATA_DIR = Path()
--- main.py
+++ main.py
@@ -1,15 +1,28 @@
 from fastapi import FastAPI
-
+from app.routes.issues import router as issues_router
 app = FastAPI()
+app.include_router(issues_router)
 
+items = [1, 2, 3]
 
 @app.get("/")
 def read_root():
   return {"message:" "Hello World"}
 
 
+@app.get("/items")
+def get_items():
+  return items
+
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q:str = None):
-  print(f"fastapi gave me item_id: {item_id}")
-  print(f"fastapi gave me a: {q}")
-  return {"item_id": item_id, "query" : q}
+def get_item(item_id: int):
+  for item in items:
+    if(item == item_id):
+      return item
+  return {"error found"}
+  
+
+@app.post("/items")
+def post_item(item : int):
+  items.append(item)
+  return items
